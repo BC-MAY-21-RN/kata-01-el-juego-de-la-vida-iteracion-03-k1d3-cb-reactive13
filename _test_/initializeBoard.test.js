3afcab9fbe1e45ed3c4bdf3ffaeb8575d657a8a6
Lucas Manuel <49735490+Lucas-aquiles@users.noreply.github.com>
2022-08-04 20:21:38 -0300

    cambios

--- _test_/initializeBoard.test.js
+++ _test_/initializeBoard.test.js
@@ -6,3 +6,9 @@ test('Adding 1 + 1 equals 2', () => {
     let test = new Board(4, 4)
     expect((test.sum(1, 1))).toBe(2)
 })
+
+test('', () => {
+    let test = new Board(4, 4)
+    expect((isNaN(test.x))).toBe(false)
+    expect((isNaN(test.y))).toBe(false)
+})
